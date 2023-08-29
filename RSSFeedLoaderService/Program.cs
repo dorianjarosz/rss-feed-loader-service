fd1505fc1b073a1f0ec2948ce6e27e32ea2ab29e
Jarosz, Dorian <dorian.jarosz@atos.net>
2023-08-29 16:21:08 +0200

    Init

---
+++ RSSFeedLoaderService/Program.cs
@@ -0,0 +1,10 @@
+using RSSFeedLoaderService;
+
+IHost host = Host.CreateDefaultBuilder(args)
+    .ConfigureServices(services =>
+    {
+        services.AddHostedService<Worker>();
+    })
+    .Build();
+
+host.Run();
